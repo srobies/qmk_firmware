a15a5742019fe0b14a92b517a7faeea207db3308
LrobieProject <sprobie1@gmail.com>
2020-05-20 01:06:31 -0500

    Small edits to config.h

--- keyboards/tada68/keymaps/lrobie/config.h
+++ keyboards/tada68/keymaps/lrobie/config.h
@@ -48,6 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 // Set time for tap dance 
+//Try PERMISSIVE_HOLD and lowering TAPPING_TERM to 200 here instead
 #define TAPPING_TERM 400
 #define IGNORE_MOD_TAP_INTERRUPT
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
