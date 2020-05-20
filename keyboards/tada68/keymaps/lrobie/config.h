7f6c798e04a514c8f2728ebeae5f4a02b1969c6e
LrobieProject <sprobie1@gmail.com>
2020-05-20 04:40:36 -0500

    Usable settings for mod-tap

--- keyboards/tada68/keymaps/lrobie/config.h
+++ keyboards/tada68/keymaps/lrobie/config.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBOUNCE 5
 // Set time for tap dance 
 //Try PERMISSIVE_HOLD and lowering TAPPING_TERM to 200 here instead
-#define TAPPING_TERM 400
+#define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
