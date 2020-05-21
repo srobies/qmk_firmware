124fd5f603a0f893bbce6e9ea582269593bcbb82
LrobieProject <sprobie1@gmail.com>
2020-05-20 22:28:49 -0500

    Fixed function layer arrow keys

--- keyboards/tada68/keymaps/lrobie/keymap.c
+++ keyboards/tada68/keymaps/lrobie/keymap.c
@@ -84,6 +84,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
   _______,_______, KC_UP,KC_MPLY,KC_MPRV, KC_MNXT,_______,_______,_______,_______,KC_RIGHT,_______,_______, _______,KC_HOME, \
   _______,KC_LEFT,KC_DOWN,KC_RIGHT,KC_MUTE,KC_VOLD,KC_VOLU,KC_LEFT,_______,_______,_______,_______,_______,KC_END, \
-  _______,_______,KC_DOWN,KC_UP,BL_TOGG, BL_DEC,BL_INC, _______,_______,_______,_______,_______, _______, _______, \
+  _______,_______,_______,KC_DOWN,KC_UP,BL_TOGG, BL_DEC,BL_INC, _______,_______,_______,_______,_______, _______,  \
   _______,_______,_______,_______,_______,_______,TO(_QWERTY_Gaming),TO(_Dvorak_Gaming),TO(_QWERTY), TO(_Dvorak)),
 };
