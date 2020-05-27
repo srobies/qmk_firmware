477a23072b855c0df99f1193329bf4f7862dd768
LrobieProject <sprobie1@gmail.com>
2020-05-27 00:27:06 -0500

    Fixed some issues with TD and RBRC/LBRC/BSLASH keys

--- keyboards/tada68/keymaps/lrobie/keymap.c
+++ keyboards/tada68/keymaps/lrobie/keymap.c
@@ -40,7 +40,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_0_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0,KC_F10),
     [TD_LBRC_F11] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC,KC_F11),
     [TD_RBRC_F12] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC,KC_F12),
-    [TD_BSLASH_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS,KC_LBRC),
+    [TD_SLASH_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH,KC_LBRC),
     [TD_EQL_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_EQL,KC_RBRC),
     [TD_MINS_F11] = ACTION_TAP_DANCE_DOUBLE(KC_MINS,KC_F11),
     [TD_EQL_F12] = ACTION_TAP_DANCE_DOUBLE(KC_EQL,KC_F12)
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Keymap _Dvorak: (Base Layer with Dvorak Layout) Default Layer
 [_Dvorak] = LAYOUT_65_ansi( 
   KC_GESC,  TD(TD_1_F1),    TD(TD_2_F2),    TD(TD_3_F3),    TD(TD_4_F4),    TD(TD_5_F5),    TD(TD_6_F6),    TD(TD_7_F7),    TD(TD_8_F8),    TD(TD_9_F9),    TD(TD_0_F10),    TD(TD_LBRC_F11), TD(TD_RBRC_F12),  KC_BSPC,KC_GRV, \
-  KC_TAB,  KC_QUOTE,   KC_COMMA,   KC_DOT,   KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLASH, TD(TD_BSLASH_LBRC), TD(TD_EQL_RBRC),KC_DEL, \
+  KC_TAB,  KC_QUOTE,   KC_COMMA,   KC_DOT,   KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   TD(TD_SLASH_LBRC), TD(TD_EQL_RBRC), KC_BSLS,KC_DEL, \
   MO(_FL), LCTL_T(KC_A),    LSFT_T(KC_O),    LGUI_T(KC_E),    LALT_T(KC_U),    LT(_FL,KC_I),    LT(_FL,KC_D),    RALT_T(KC_H),    RGUI_T(KC_T),    RSFT_T(KC_N),    RCTL_T(KC_S) ,KC_MINS,         KC_ENT,KC_PGUP,  \
   KC_LSFT,         KC_SCLN,   KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,KC_V, KC_Z,   KC_RSFT,KC_UP,KC_PGDN, \
   KC_LCTL, KC_LGUI,KC_LALT,                KC_SPACE,                        KC_RALT,TG(_FL),TD(TD_RCTRL_CAPS), KC_LEFT,KC_DOWN,KC_RGHT),
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Keymap _Dvorak_Gaming: (Layer with Dvorak Layout and no mod-tap)
 [_Dvorak_Gaming] = LAYOUT_ansi( 
   KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,  KC_BSPC,KC_GRV, \
-  KC_TAB,  KC_QUOTE,   KC_COMMA,   KC_DOT,   KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLASH, TD(TD_BSLASH_LBRC), TD(TD_EQL_RBRC),KC_DEL, \
+  KC_TAB,  KC_QUOTE,   KC_COMMA,   KC_DOT,   KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   TD(TD_SLASH_LBRC), TD(TD_EQL_RBRC), KC_BSLS,KC_DEL, \
   MO(_FL), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S ,KC_MINS,         KC_ENT,KC_PGUP,  \
   KC_LSPO,         KC_SCLN,   KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,KC_V, KC_Z,   KC_RSPC,KC_UP,KC_PGDN, \
   KC_LCTL, KC_LGUI,KC_LALT,                KC_SPACE,                        KC_RALT,TG(_FL),TD(TD_RCTRL_CAPS), KC_LEFT,KC_DOWN,KC_RGHT),
