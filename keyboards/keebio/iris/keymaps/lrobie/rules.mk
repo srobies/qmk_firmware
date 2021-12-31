e3131d2a386247425465a7bb38bc0b39547ba45d
srobies <sprobie1@gmail.com>
2021-12-31 02:43:59 -0600

    Change to raise/lower layout

--- keyboards/keebio/iris/keymaps/lrobie/rules.mk
+++ keyboards/keebio/iris/keymaps/lrobie/rules.mk
@@ -10,4 +10,3 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-TAP_DANCE_ENABLE = yes
