d580a077af08a82781b30aa088a4f44d20a2b6ab
srobies <sprobie1@gmail.com>
2022-03-20 23:31:46 -0500

    Planck ergo changes

--- keyboards/planck/keymaps/lrobie/keymap.c
+++ keyboards/planck/keymaps/lrobie/keymap.c
@@ -63,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, KC_END,  KC_PGDN, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, KC_LBRC, KC_END,  KC_PGDN, KC_RBRC, _______, _______, _______, _______,
     _______, _______, _______, _______, MO_AD,   _______, _______, _______, _______, _______, _______, _______
 ),
 [_LOWER] = LAYOUT_planck_grid(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     _______, _______, KC_UP,   _______, KC_VOLU, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,
-    _______, KC_LEFT, KC_DOWN, _______, KC_VOLD, _______, _______, _______, KC_ENT,  KC_4,    KC_5,    KC_6,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, _______, _______, _______, KC_ENT,  KC_4,    KC_5,    KC_6,
     KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_MUTE, _______, _______, MO_AD,   KC_0,    KC_1,    KC_2,    KC_3
 ),
 
