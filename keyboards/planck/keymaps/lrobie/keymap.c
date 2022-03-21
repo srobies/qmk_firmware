172565ef7c3164cf2ef36b25fe3063e0fb2cb297
srobies <59497019+srobies@users.noreply.github.com>
2022-03-20 22:32:44 -0500

    Change bspc key

--- keyboards/planck/keymaps/lrobie/keymap.c
+++ keyboards/planck/keymaps/lrobie/keymap.c
@@ -32,7 +32,7 @@ enum planck_keycodes {
 };
 
 #define LOWER LT(_LOWER, KC_ENT)
-#define RAISE LT(_RAISE, KC_BSPC)
+#define RAISE MO(_RAISE)
 
 #define TO_DV TO(_DVORAK)
 #define TO_QW TO(_QWERTY)
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_GESC, KC_QUOT, KC_COMM, KC_DOT, KC_P,  KC_Y,   KC_F,   KC_G,  KC_C,    KC_R,    KC_L,  KC_SLSH,
     KC_TAB,  KC_A,    KC_O,    KC_E,   KC_U,  KC_I,   KC_D,   KC_H,  KC_T,    KC_N,    KC_S,  KC_MINS,
     KC_LSFT, KC_SCLN, KC_Q,    KC_J,   KC_K,  KC_X,   KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, QTILE,  LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT, QTILE,  LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_LEFT, KC_RGHT,_______
 ),
 [_GAMING] = LAYOUT_planck_grid(
     QK_GESC, KC_1,    KC_2,    KC_3,   KC_4, KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC,
